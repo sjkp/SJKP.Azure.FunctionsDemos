dbcc7f45a45c8ea22e584ddbfa2509f4f3454978
Simon Pedersen <mail@sjkp.dk>
2017-03-05 13:54:54 +0100

    added precompiled function

---
+++ SJKP.PrecompiledFunctions/Functions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace SJKP.PrecompiledFunctions
+{
+    public class Functions
+    {
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, ExecutionContext ctx, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+        }
+    }
+}
